147fee553276dd6dd4c0a3556d53e405b0fb5ee4
Dmitriy One <checordog@gmail.com>
2018-06-18 20:59:48 +0300

    Initial commit

---
+++ travis_scripts/deploy_to_docker_hub.sh
@@ -0,0 +1,8 @@
+echo "Pushing service docker images to docker hub ...."
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker push motoguard/authservice:$BUILD_NAME
+docker push motoguard/rawdataservice:$BUILD_NAME
+docker push motoguard/watchservice:$BUILD_NAME
+docker push motoguard/configservice:$BUILD_NAME
+docker push motoguard/discoveryservice:$BUILD_NAME
+docker push motoguard/routingservice:$BUILD_NAME
---
+++ travis_scripts/deploy_to_mcs_cloud.sh
@@ -0,0 +1,2 @@
+echo "Launching $BUILD_NAME IN K8s"
+kompose up -f docker/common/docker-compose.yml
