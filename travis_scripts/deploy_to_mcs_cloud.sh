c955b7ad3a07b97a037cc3855510bbcc29a6e410
Dmitriy One <checordog@gmail.com>
2018-06-19 15:28:21 +0300

    travis deploy changes

--- travis_scripts/deploy_to_mcs_cloud.sh
+++ travis_scripts/deploy_to_mcs_cloud.sh
@@ -1,4 +1,2 @@
 echo "Launching $BUILD_NAME IN K8s"
-docker run -v ${TRAVIS_BUILD_DIR}/kubernetes:/kube smesch/kubectl kubectl --kubeconfig /kube/kubeconfig delete ns motoguard
-docker run -v ${TRAVIS_BUILD_DIR}/kubernetes:/kube smesch/kubectl kubectl --kubeconfig /kube/kubeconfig create ns motoguard
 docker run -v ${TRAVIS_BUILD_DIR}/kubernetes:/kube smesch/kubectl kubectl --kubeconfig /kube/kubeconfig apply -f kube --namespace=motoguard
