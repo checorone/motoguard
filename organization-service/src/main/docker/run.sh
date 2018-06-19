9cca67c521d37a7e1918c089d92a8f4de44696fc
Dmitriy One <checordog@gmail.com>
2018-06-19 23:47:51 +0300

    some

--- organization-service/src/main/docker/run.sh
+++ organization-service/src/main/docker/run.sh
@@ -5,12 +5,13 @@ getPort() {
 
 echo "********************************************************"
 echo "Waiting for the eureka server to start on port $(getPort $EUREKASERVER_PORT)"
+echo $EUREKASERVER_PORT
 echo "********************************************************"
 while ! `nc -z eurekaserver  $(getPort $EUREKASERVER_PORT)`; do sleep 3; done
 echo "******* Eureka Server has started"
 
 echo "********************************************************"
-echo "Waiting for the database server to start on port $DATABASESERVER_PORT"
+echo "Waiting for the database server to start on port $(getPort $DATABASESERVER_PORT)"
 echo "********************************************************"
 while ! `nc -z database $(getPort $DATABASESERVER_PORT)`; do sleep 3; done
 echo "******** Database Server has started "
