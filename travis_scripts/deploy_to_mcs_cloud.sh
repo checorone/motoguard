5ceb40e8be8e85a0154a2dbd314c8be09bc12364
Dmitriy One <checordog@gmail.com>
2018-06-19 21:40:22 +0300

    disabled validation

--- travis_scripts/deploy_to_mcs_cloud.sh
+++ travis_scripts/deploy_to_mcs_cloud.sh
@@ -1,3 +1,3 @@
 echo "Launching $BUILD_NAME IN K8s"
-docker run -v ${TRAVIS_BUILD_DIR}/kubernetes:/kube smesch/kubectl kubectl --kubeconfig /kube/kubeconfig apply -f kube/bootstrap.yaml
-docker run -v ${TRAVIS_BUILD_DIR}/kubernetes:/kube smesch/kubectl kubectl --kubeconfig /kube/kubeconfig apply -f kube/common --namespace=motoguard
+docker run -v ${TRAVIS_BUILD_DIR}/kubernetes:/kube smesch/kubectl kubectl --kubeconfig /kube/kubeconfig apply -f kube/bootstrap.yaml --validate=false
+docker run -v ${TRAVIS_BUILD_DIR}/kubernetes:/kube smesch/kubectl kubectl --kubeconfig /kube/kubeconfig apply -f kube/common --validate=false --namespace=motoguard 
