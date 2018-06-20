debd087168db5139170ed28c5915fac6194175ab
Dmitriy One <checordog@gmail.com>
2018-06-20 21:44:15 +0300

    ingress added

--- travis_scripts/deploy_to_mcs_cloud.sh
+++ travis_scripts/deploy_to_mcs_cloud.sh
@@ -1,3 +1,4 @@
 echo "Launching $BUILD_NAME IN K8s"
-docker run -v ${TRAVIS_BUILD_DIR}/kubernetes:/kube smesch/kubectl kubectl --kubeconfig /kube/kubeconfig apply -f kube/bootstrap.yaml --validate=false
-docker run -v ${TRAVIS_BUILD_DIR}/kubernetes:/kube smesch/kubectl kubectl --kubeconfig /kube/kubeconfig apply -f kube/common --validate=false --namespace=motoguard 
+docker run -v ${TRAVIS_BUILD_DIR}/kubernetes:/kube smesch/kubectl kubectl --kubeconfig /kube/kubeconfig apply -f kube/before-deploy --validate=false
+docker run -v ${TRAVIS_BUILD_DIR}/kubernetes:/kube smesch/kubectl kubectl --kubeconfig /kube/kubeconfig apply -f kube/deploy --validate=false --namespace=motoguard 
+docker run -v ${TRAVIS_BUILD_DIR}/kubernetes:/kube smesch/kubectl kubectl --kubeconfig /kube/kubeconfig apply -f kube/after-deploy --validate=false --namespace=motoguard 
