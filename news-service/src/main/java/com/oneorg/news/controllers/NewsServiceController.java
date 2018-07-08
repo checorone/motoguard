a87b08e5c74513cfcc7cb8af32a71bde83aac18f
Dmitriy One <checordog@gmail.com>
2018-07-09 00:28:13 +0300

    fixed news service

--- news-service/src/main/java/com/oneorg/news/controllers/NewsServiceController.java
+++ news-service/src/main/java/com/oneorg/news/controllers/NewsServiceController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
 @RequestMapping(value="v1")
